3c6c6327439a930ae19c0358317bd8d7f91df08c
venkat9573-avs <venkatavs9573@gmail.com>
2024-07-18 07:34:24 +0530

    In header small changes are made

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
 function downloadResume( ){
     const link = document.createElement('a');
-    link.href = 'venkata_sai_kalyan_resume.docx';
-    link.download = 'venkata_sai_kalyan_resume.docx';
+    link.href = 'venkata_sai_kalyan_resume.pdf';
+    link.download = 'venkata_sai_kalyan_resume.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
